028d070577094cc171dadb142cdb4b8c82b4549a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-23 11:08:03 +0400

    6 employees action is pushed in the stack

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -3,6 +3,13 @@ import java.util.Stack;
 public class EmployeeStackSearch {
     public static void main(String[] args){
         Stack<String> employeeAction = new Stack<>();
+        employeeAction.push("Enter");
+        employeeAction.push("Leave");
+        employeeAction.push("Work");
+        employeeAction.push("Write");
+        employeeAction.push("Submit");
+        employeeAction.push("Remove");
+
 
     }
 }
