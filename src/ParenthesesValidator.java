90627f663bc70ecd5e9593278e3df625e805cf64
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-23 11:45:36 +0400

    5 characters are added

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -3,6 +3,12 @@ import java.util.Stack;
 public class ParenthesesValidator {
     public static void main(String[] args){
         Stack<String>  characters = new Stack<>();
+        characters.push("<>");
+        characters.push("[]");
+        characters.push("{}");
+        characters.push("()");
+        characters.push("//");
+
 
     }
 }
