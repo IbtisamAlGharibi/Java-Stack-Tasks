107ee1435c90a81aa5b562010641c36acd63909c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-23 11:33:19 +0400

    5 elements pushed in the stack

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -3,6 +3,12 @@ import java.util.Stack;
 public class BrowserHistorySimulator {
     public static void main(String[] args){
         Stack<String> webPages = new Stack<>();
+        webPages.push("Amazon");
+        webPages.push("Shein");
+        webPages.push("AliExpress");
+        webPages.push("Slack");
+        webPages.push("ClickUp");
+
 
 
     }
