5bfe53b4575003d623d569f1f002c1df56878177
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-26 11:52:47 +0400

    the method is called in the main method

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -2,9 +2,8 @@ import java.util.Stack;
 
 public class TextEditorWithUndoRedo {
     public static void main(String[] args){
-        Stack<String> undo = new Stack<>();
-        Stack<String> redo = new Stack<>();
-        StringBuilder builder = new StringBuilder();
+        String text = "Hello";
+                type(text);
 
     }
 
