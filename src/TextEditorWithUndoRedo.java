e1a84700a0de511843c17745c74f95bf3f8fa9b5
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-26 11:24:37 +0400

    creating string builder

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -4,7 +4,7 @@ public class TextEditorWithUndoRedo {
     public static void main(String[] args){
         Stack<String> undo = new Stack<>();
         Stack<String> redo = new Stack<>();
-
+        StringBuilder builder = new StringBuilder();
 
     }
 }
