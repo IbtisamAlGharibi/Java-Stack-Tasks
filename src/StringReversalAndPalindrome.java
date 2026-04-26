a0583275e450326d6d206c219742e35543ffce89
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-26 09:01:21 +0400

    the stack is created

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -1,5 +1,9 @@
+import java.util.Stack;
+
 public class StringReversalAndPalindrome {
     public static void main(String[] args){
+        Stack<Character> characters = new Stack<>();
+
 
     }
 }
