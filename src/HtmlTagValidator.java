5231937f67f037a017143dae64dbe0a4c0a0a729
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-26 11:57:17 +0400

    The stack is created

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
 public class HtmlTagValidator {
+    public static void main(String[] args){
+
+
+        Stack<String> tags = new Stack<>();
+
+    }
 }
