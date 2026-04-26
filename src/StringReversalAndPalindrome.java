626022b6b4a408af6e67e9c548a22add8d43c3f9
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-26 09:05:05 +0400

    5 strings are created

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -3,6 +3,12 @@ import java.util.Stack;
 public class StringReversalAndPalindrome {
     public static void main(String[] args){
         Stack<Character> characters = new Stack<>();
+        String str1 = "mom";
+        String str2= "dad";
+        String str3 = "brother";
+        String str4 = "sister";
+        String str5 = "aunt";
+
 
 
     }
