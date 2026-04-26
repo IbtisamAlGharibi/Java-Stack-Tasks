6a9ad24956e035ef223d2a09babd79b33778f444
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-26 10:16:17 +0400

    5 postfix are created

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -3,5 +3,11 @@ import java.util.Stack;
 public class PostfixEvaluator {
     public static void main(String[] args){
         Stack<Integer> operands = new Stack<>();
+        String str1 = " 2 3 -";
+        String str2 = "2 3 + -";
+        String str3 = "4 5 / 4 *";
+        String str4 = "5 6 + % -";
+        String str5 = " / 5 * 7 8 +";
+
     }
 }
