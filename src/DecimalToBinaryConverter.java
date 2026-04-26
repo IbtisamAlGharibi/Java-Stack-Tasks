7238b84196e198ce1ea57de7b68265d0ef1033e2
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-26 09:31:25 +0400

    5 double numbers are written

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -3,6 +3,11 @@ import java.util.Stack;
 public class DecimalToBinaryConverter {
     public static void main(String[] args){
         Stack<Integer> remainders = new Stack<>();
+        double num1 = 2.1;
+        double num2 = 3.2;
+        double num3 = 4.5;
+        double num4 = 5.3;
+        double num5 = 8.2;
 
     }
 
