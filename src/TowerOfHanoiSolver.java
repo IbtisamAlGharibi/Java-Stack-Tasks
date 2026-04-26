402427cb673635bf3910a9428d58629e6a0b7d2a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-26 13:40:17 +0400

    3 stacks are created

--- src/TowerOfHanoiSolver.java
+++ src/TowerOfHanoiSolver.java
@@ -1,4 +1,9 @@
+import java.util.Stack;
+
 public class TowerOfHanoiSolver {
+    static Stack<String> A = new Stack<>();
+    static Stack<String> B = new Stack<>();
+    static Stack<String> C = new Stack<>();
     public static void main(String[] args){
 
     }
