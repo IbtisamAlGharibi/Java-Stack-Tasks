dac7e4d5fe5cd8ed0d584003c02323e539918e4f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-26 12:01:05 +0400

    5 html tags strings is created

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -2,9 +2,13 @@ import java.util.Stack;
 
 public class HtmlTagValidator {
     public static void main(String[] args){
-
-
         Stack<String> tags = new Stack<>();
+        String str1 = "<html> </html>";
+        String str2 = "<head> </head>";
+        String str3 = "<body> </body>";
+        String str4 = "<p> </p>";
+        String str5 = "<header> </header>";
+
 
     }
 }
