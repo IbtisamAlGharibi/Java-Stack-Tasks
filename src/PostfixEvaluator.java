a135452ec3d39c2720fc8951ff77878211c06f80
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-26 10:34:07 +0400

    Task 8: Evaluate Postfix Expressions Using Java Stack

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -3,11 +3,11 @@ import java.util.Stack;
 public class PostfixEvaluator {
     public static void main(String[] args){
         Stack<Integer> operands = new Stack<>();
-        String str1 = " 2 3 -";
-        String str2 = "2 3 + -";
+        String str1 = "2 3 -";
+        String str2 = "2 3 +";
         String str3 = "4 5 / 4 *";
-        String str4 = "5 6 + % -";
-        String str5 = " / 5 * 7 8 +";
+        String str4 = "5 6 + 2 %";
+        String str5 = "5 7 8 + *";
 
         System.out.println(evaluatePostfix(str1));
         System.out.println(evaluatePostfix(str2));
@@ -41,5 +41,7 @@ public class PostfixEvaluator {
             }
         }
         return stack.pop();
+
+
     }
 }
