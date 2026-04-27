3ec3e7360360f1c69fac423920342625e9c7bc9e
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-27 14:42:32 +0400

    2 stacks are created

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,4 +1,9 @@
+import java.util.Stack;
+
 public class MinStack {
+    static Stack<String> mainStack = new Stack<>();
+    static Stack<String> minStack = new Stack<>();
+
     public static void main(String[] args){
 
     }
