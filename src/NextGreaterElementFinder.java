d54742a0a50bb2eafdff800faac6e234920c3cd5
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-27 11:16:51 +0400

    stack and 6 test arrays are created

--- src/NextGreaterElementFinder.java
+++ src/NextGreaterElementFinder.java
@@ -1,6 +1,15 @@
-public class NextGreaterElementFinder {
-    public static void main(String[] args){
+import java.util.Stack;
 
+public class NextGreaterElementFinder {
+    static Stack<Integer> arrays = new Stack<>();
+
+    public static void main(String[] args){
+        int[] arr1 = {1,2,3};
+        int[] arr2 ={2,3,4};
+        int[] arr3 = {4,5,2};
+        int[] arr4 = {1,6,9};
+        int[] arr5 ={2,8,9,3};
+        int[] arr6 = {4,8,6,0};
 
     }
 }
