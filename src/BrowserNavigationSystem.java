0a25d128a2de60dcd5aa68d5220d06c9d682f359
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-27 09:12:46 +0400

    String current page is declared

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -3,6 +3,7 @@ import java.util.Stack;
 public class BrowserNavigationSystem {
     static   Stack<String> backStack = new Stack<>();
     static  Stack<String> forwardStack = new Stack<>();
+    static String currentPage = "";
     public static void main( String[] args){
 
     }
