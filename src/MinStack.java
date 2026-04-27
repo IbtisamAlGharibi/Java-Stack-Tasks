c58b49dbb45d504c8dddcb6c0c34bc1940b7f1e9
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-27 14:56:38 +0400

    4 elements are pushed using push method

--- src/MinStack.java
+++ src/MinStack.java
@@ -5,6 +5,10 @@ public class MinStack {
     static Stack<Integer> minStack = new Stack<>();
 
     public static void main(String[] args){
+        push(1);
+        push(2);
+        push(3);
+        push(4);
 
     }
 
@@ -13,12 +17,16 @@ public class MinStack {
         if (minStack.isEmpty() ||  value <= minStack.peek()){
             minStack.push(value);
         }
+        System.out.println("min stack: " + minStack);
+        System.out.println("Main stack: " + mainStack);
     }
 
     public static void pop(){
         int fromMain = mainStack.pop();
         if (fromMain == minStack.peek()){
             minStack.pop();
+            System.out.println("min stack: " + minStack);
+            System.out.println("Main stack: " + mainStack);
         }
     }
 
