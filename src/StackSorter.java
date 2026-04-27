ff0d45f50c9cc8ec88538a2198b032ed107b3bb3
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-27 15:39:35 +0400

    2 stacks are created

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -1,4 +1,8 @@
+import java.util.Stack;
+
 public class StackSorter {
+    static Stack<Integer> original = new Stack<>();
+    static Stack<Integer> temporary = new Stack<>();
     public static void main(String[] args){
 
     }
