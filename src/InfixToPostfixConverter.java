3dd87dafe1962ad613c5d39035087e1ff45b8d92
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-27 09:48:20 +0400

    new method is created

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -4,6 +4,9 @@ public class InfixToPostfixConverter {
     static Stack<String > operators = new Stack<>();
     public static void main(String[] args){
 
+    }
+
+    public static void infixToPostfix(String infix){
 
     }
 }
