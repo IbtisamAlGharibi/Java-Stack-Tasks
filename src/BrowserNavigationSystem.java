f363e68abe1da3d82b11c567de184f1a2af75539
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-27 09:11:11 +0400

    2 stacks are created

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -1,4 +1,8 @@
+import java.util.Stack;
+
 public class BrowserNavigationSystem {
+    static   Stack<String> backStack = new Stack<>();
+    static  Stack<String> forwardStack = new Stack<>();
     public static void main( String[] args){
 
     }
