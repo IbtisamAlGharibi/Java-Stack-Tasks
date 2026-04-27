9f97e432c643e1ccd61aed192ec101babbe00984
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-27 14:45:27 +0400

    new method is created to push values in the stacks

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,10 +1,18 @@
 import java.util.Stack;
 
 public class MinStack {
-    static Stack<String> mainStack = new Stack<>();
-    static Stack<String> minStack = new Stack<>();
+    static Stack<Integer> mainStack = new Stack<>();
+    static Stack<Integer> minStack = new Stack<>();
 
     public static void main(String[] args){
+
+    }
+
+    public static void push(int value){
+        mainStack.push(value);
+        if (minStack.isEmpty() ||  value <= minStack.peek()){
+            minStack.push(value);
+        }
 
     }
 }
