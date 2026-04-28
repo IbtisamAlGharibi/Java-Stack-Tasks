018420baf40672fec14b6910a5c0244279c4cf26
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-28 14:34:58 +0400

    while loop is created

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -10,8 +10,9 @@ public class NQueensSolver {
             this.col = col;
         }
     }
-    public static void main(String[] args){
 
+    public static void main(String[] args){
+        //solveNQueens(4);
     }
 
     public static void solveNQueens(int n) {
@@ -24,6 +25,14 @@ public class NQueensSolver {
         int row = 0;
         int col = 0;
         int count = 0;
+
+        while (true) {
+            boolean found = false;
+            for (; col < n; col++) {
+                Position p = new Position(row, col);
+
+            }
+        }
     }
 
     private static boolean isSafe(Position pos, Stack<Position> queens) {
@@ -31,9 +40,6 @@ public class NQueensSolver {
             if (q.col == pos.col) return false;
             if (Math.abs(q.row - pos.row) == Math.abs(q.col - pos.col)) return false;
         }
-
         return true;
     }
-
 }
-
