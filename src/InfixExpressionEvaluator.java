1f9e3dfb077ae7faad5e700b6515392fffdb688f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-28 08:52:53 +0400

    2 stacks are created

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -1,4 +1,9 @@
+import java.util.Stack;
+
 public class InfixExpressionEvaluator {
+    static Stack<Double> operandStack = new Stack<Double>();
+    static  Stack<Character> operatorStack = new Stack<>();
+
     public static void main(String[] args){
 
     }
