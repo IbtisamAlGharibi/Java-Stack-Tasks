e415ff0582ac5b5a95eacb7cb7aaaf78d4e2be09
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-28 09:59:49 +0400

    the stack is created

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
 public class LargestRectangleInHistogram {
+    static Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args){
+
+
+    }
+
+
 }
