28c6397dae8893fcab31c8ebc07a75eccbc65d2f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-28 12:29:53 +0400

    for loop is created

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -43,7 +43,18 @@ public class LargestRectangleInHistogram {
     }
 
     public static void displayHistogram(int[] heights){
+        System.out.println("Histogram:");
 
+        int max = 0;
+        for (int h : heights) if (h > max) max = h;
+
+        for (int i = max; i > 0; i--) {
+            for (int j = 0; j < heights.length; j++) {
+                if (heights[j] >= i) System.out.print("# ");
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
     }
 
 }
