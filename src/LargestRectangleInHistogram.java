0070287f072744f2c013b52cf7325abdd4a62140
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-28 12:27:51 +0400

    if statement is written to calculate the area

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -2,24 +2,43 @@ import java.util.Stack;
 
 public class LargestRectangleInHistogram {
     static Stack<Integer> stack = new Stack<>();
+
     public static void main(String[] args){
 
-
     }
+
     public static int findLargestRectangle(int[] heights){
         if (heights.length == 0){
             return 0;
         }
+
+        int max = 0;
+
         for (int i = 0; i <= heights.length; i++) {
+
             while (!stack.isEmpty() &&
                     (i == heights.length || heights[i] < heights[stack.peek()])) {
 
+                int h = heights[stack.pop()];
+                int width;
+
+                if (stack.isEmpty()) width = i;
+                else width = i - stack.peek() - 1;
+
+                int area = calculateArea(heights, 0, width, h);
+
+                if (area > max) max = area;
+
+                System.out.println("Stack: " + stack + " Height: " + h + " Width: " + width + " Area: " + area);
             }
 
+            stack.push(i);
         }
+
+        return max;
     }
 
     public static int calculateArea(int[] heights, int start, int end, int height){
-
+        return height * end;
     }
 }
